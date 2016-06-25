68a3ac4f6ba5aab1dd2078b2f81c96771a2b7e54
lyushninaelena <lyushnina@gmail.com>
2016-06-25 22:28:05 +0300

    Создан класс основной структуры (граф)

---
+++ src/Graph_interface.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Lyushnina Elena on 25.06.2016.
+ */
+public interface Graph_interface {
+    /**
+     * Удаляет все вершины и ребра из графа.
+     */
+    void clear();
+
+    /**
+     * Добавляет ребро в граф
+     *
+     * @param _x      начало дуги
+     * @param _y      конец дуги
+     * @param _weight вес дуги
+     */
+    void addEdge(int _x, int _y, int _weight);
+}
